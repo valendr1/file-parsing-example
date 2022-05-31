e4c8a33cc2918549e8e9604fbf9471c66126c797
Valery <valerijvaler931@gmail.com>
2022-05-31 23:47:57 +0500

    added Allure

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,14 @@
+package tests;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+
+    @BeforeAll
+    static void startup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+}
